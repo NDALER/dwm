2ad2608ef3536889eb81452a1bd08d09eea9caba
NDALER <sdalvianto@protonmail.com>
2022-03-16 21:43:10 +0800

    patching titlecolor

--- config.h
+++ config.h
@@ -25,6 +25,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTitle]  = { col_gray4, col_cyan,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
